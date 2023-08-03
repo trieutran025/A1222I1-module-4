21b78ea141e7da454c743dd44cf0410b0624f165
Tran Le Quang Trieu <trieutran025@gmail.com>
2023-08-03 15:23:40 +0700

    ex_ss02

--- ss02_spring_mvc_controller/exercise/myCaculator/src/main/java/codegym/vn/controller/MyCalculatorController.java
+++ ss02_spring_mvc_controller/exercise/myCaculator/src/main/java/codegym/vn/controller/MyCalculatorController.java
@@ -15,24 +15,27 @@ public class MyCalculatorController {
 
     @PostMapping("/calculate")
     public String calculator(Model model, @RequestParam("calculations") String calculations,
-                             @RequestParam("firstNum") int firstNum,, @RequestParam("secondNum") int secondNum) {
-        int result = 0;
+                             @RequestParam("firstNum") int firstNum, @RequestParam("secondNum") int secondNum) {
+        double result = 0;
         switch (calculations) {
-            case "add":
-                result = firstNum + secondNum;
+            case "addition(+)":
+                result = (double) firstNum + secondNum;
                 break;
-            case "sub":
-                result = firstNum - secondNum;
+            case "subtraction(-)":
+                result = (double) firstNum - secondNum;
                 break;
-            case "mul":
-                result = firstNum  * secondNum;
+            case "multiplication(X)":
+                result =(double) firstNum  * secondNum;
                 break;
-            case "div":
-                result = firstNum / secondNum;
+            case "division(/)":
+                result = (double) firstNum / secondNum;
                 break;
             default:
-                
         }
+        model.addAttribute("calculations",calculations);
+        model.addAttribute("firstNum",firstNum);
+        model.addAttribute("secondNum",secondNum);
+        model.addAttribute("result",result);
         return "/index";
     }
 }
--- ss02_spring_mvc_controller/exercise/myCaculator/src/main/webapp/WEB-INF/views/index.jsp
+++ ss02_spring_mvc_controller/exercise/myCaculator/src/main/webapp/WEB-INF/views/index.jsp
@@ -7,10 +7,19 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>My Calculator</title>
+</head>
+<body>
+<form method="post" action="/calculate">
+
+    <input type="number" name="firstNum" value="${firstNum}">
+    <input type="number" name="secondNum" value="${secondNum}"> <br><br>
+    <input type="submit" value="addition(+)" name="calculations">
+    <input type="submit" value="subtraction(-)" name="calculations">
+    <input type="submit" value="multiplication(X)" name="calculations">
+    <input type="submit" value="division(/)" name="calculations">
+    <h1> Result ${calculations} : ${result}</h1>
+</form>
+</body>
 </html>
